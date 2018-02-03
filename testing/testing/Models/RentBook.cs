b5de0385af91663a1b6b5909f2441df164e192ca
apk2134 <mailanupamprakash@gmail..com>
2018-02-03 21:02:03 +0530

    Intial Comit

---
+++ testing/testing/Models/RentBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace testing.Models
+{
+    public class RentBook
+    {
+    }
+    public class admin
+    {
+        [Required(ErrorMessage = "Please Provide Username", AllowEmptyStrings = false)]
+        public string Username
+        {
+            get;
+            set;
+        }
+        [Required(ErrorMessage = "Please provide password", AllowEmptyStrings = false)]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ testing/testing/RentBook/RentBookDAL.cs
@@ -0,0 +1,65 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace testing.RentBook
+{
+    public class RentBookDAL
+    {
+        private bool connection_open;
+        private MySqlConnection connection;
+        public RentBookDAL()
+        {
+            Get_Connection();
+        }
+        private void Get_Connection()
+        {
+            connection_open = false;
+
+            connection = new MySqlConnection();
+            //connection = DB_Connect.Make_Connnection(ConfigurationManager.ConnectionStrings["SQLConnection"].ConnectionString);
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+
+            //            if (db_manage_connnection.DB_Connect.OpenTheConnection(connection))
+            if (Open_Local_Connection())
+            {
+                connection_open = true;
+            }
+            else
+            {
+                //					MessageBox::Show("No database connection connection made...\n Exiting now", "Database Connection Error");
+                //					 Application::Exit();
+            }
+
+        }
+
+        private bool Open_Local_Connection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public string test()
+        {
+            string i;
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText =
+            string.Format("select UserName from login; ");
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            i= reader.GetString(0); 
+            return i;
+        }
+    }
+}
